ca402381a7d21205bac1889cd537d0e0e7e29ef4
ameerlaeeqa <ameerlaeeqa@gmail.com>
2026-01-03 07:23:40 +0200

    sw.js

---
+++ sw.js
@@ -0,0 +1,12 @@
+const CACHE_NAME = "safari-drive-v1";
+const ASSETS = ["./", "./index.html", "./style.css", "./app.js", "./manifest.json"];
+
+self.addEventListener("install", (e) => {
+  e.waitUntil(caches.open(CACHE_NAME).then((c) => c.addAll(ASSETS)));
+});
+
+self.addEventListener("fetch", (e) => {
+  e.respondWith(
+    caches.match(e.request).then((cached) => cached || fetch(e.request))
+  );
+});
